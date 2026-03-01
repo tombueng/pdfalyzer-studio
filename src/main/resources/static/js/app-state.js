ad14202d8085cc450d6e7fa7a6638c4ec93c11b1
Thomas Büngener <thomas@buengener.de>
2026-03-01 14:16:21 +0100

    Fix: Enhance form field deletion process with undo functionality and improve UI consistency

--- src/main/resources/static/js/app-state.js
+++ src/main/resources/static/js/app-state.js
@@ -15,6 +15,7 @@ var PDFalyzer = (function () {
             editFieldType: null,
             pendingFormAdds: [],
             pendingFieldRects: [],
+            pendingFieldDeletes: {},
             selectedFieldNames: [],
             pageViewports: [],
             pageCanvases: [],
